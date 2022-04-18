73aaad7f569a0cf21daf1c81d6c74255a0cdd87d
SonyHotchandani11 <89579428+Sony672@users.noreply.github.com>
2022-04-18 12:23:48 +0530

    Delete myscript.js

--- myscript.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-    // register service worker
-    navigator.serviceWorker.register('service-worker.js')
-    .then((reg) => console.log("service worker registered.", reg))
-    .catch((err) =>console.log("service worker not registered", err))
-}
